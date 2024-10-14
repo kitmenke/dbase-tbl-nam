4ddfc49ec11de67beedbab0d1a3053349fe022f4
Kit Menke <cwmenke@gmail.com>
2024-10-13 21:03:18 -0500

    Add uppercase/lowercase toggle.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,25 @@
 <script>
     import '../lib/abbreviations.js';
     import { abbreviations } from '../lib/abbreviations.js';
-    let inputName = 'create date';
-    $: outputName = inputName.split(' ').map(word => abbreviations[word.toUpperCase()]).join('_');
+    let chkIsUppercase = true;
+    let txtInputName = 'create date';
+    $: outputName = convert(txtInputName, chkIsUppercase);
+
+    function convert(){
+        let words = txtInputName.split(' ');
+        let result = words.map(word => abbreviations.hasOwnProperty(word.toUpperCase()) ? abbreviations[word.toUpperCase()] : '?').join('_');
+        return chkIsUppercase ? result.toUpperCase() : result.toLowerCase();
+    }
 </script>
 
 <label>
     Name:
-    <input type="text" bind:value={inputName} />
+    <input type="text" bind:value={txtInputName} />
 </label>
 <div>{outputName}</div>
 
+<div>
+    Options:
+    <label><input type="checkbox" bind:checked={chkIsUppercase} />{chkIsUppercase ? 'UPPERCASE' : 'lowercase'}</label>
+    
+</div>
