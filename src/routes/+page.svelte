1b1f068d766f69372e85cb61c6ff7dac283dacbd
Kit Menke <cwmenke@gmail.com>
2024-11-16 09:44:08 -0600

    Add two columns with textareas to allow multiple lines

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,36 @@
 <script>
     import '../lib/abbreviations.js';
     import { abbreviations } from '../lib/abbreviations.js';
-    let chkIsUppercase = true;
-    let txtInputName = 'create date';
-    $: outputName = convert(txtInputName, chkIsUppercase);
+    import 'tachyons';
 
-    function convert(){
-        let words = txtInputName.split(' ');
+    let chkIsUppercase = false;
+    let txtFriendlyNames = 'create date\nupdate date';
+    $: txtDatabaseNames = convert(txtFriendlyNames, chkIsUppercase);
+
+    function convertLine(line){
+        let words = line.split(' ');
         let result = words.map(word => abbreviations.hasOwnProperty(word.toUpperCase()) ? abbreviations[word.toUpperCase()] : '?').join('_');
         return chkIsUppercase ? result.toUpperCase() : result.toLowerCase();
     }
+
+    function convert(){
+        let lines = txtFriendlyNames.split('\n');
+        return lines.map(line => convertLine(line)).join('\n');
+    }
 </script>
 
-<label>
-    Name:
-    <input type="text" bind:value={txtInputName} />
-</label>
-<div>{outputName}</div>
-
-<div>
-    Options:
-    <label><input type="checkbox" bind:checked={chkIsUppercase} />{chkIsUppercase ? 'UPPERCASE' : 'lowercase'}</label>
-    
-</div>
+<form class="cf pa4 black-80">
+    <div class="fl w-100 pa3 bg-light-gray">
+        Options:
+        <label class="pa0 ma0 lh-copy f6 pointer"><input type="checkbox" bind:checked={chkIsUppercase} />{chkIsUppercase ? 'UPPERCASE' : 'lowercase'}</label>
+    </div>
+    <div class="fl w-50 tc pa3">
+        <label for="friendly-names" class="f6 b db mb2">Enter the friendly names to convert:</label>
+        <textarea name="friendly-names" bind:value={txtFriendlyNames} class="db border-box w-100 ba b--black-20 br2 mb2 pa2"></textarea>
+    </div>
+    <div class="fl w-50 tc pa3">
+        <label for="database-names" class="f6 b db mb2">Database Names</label>
+        <textarea readonly name="database-names" bind:value={txtDatabaseNames} class="db border-box w-100 ba b--black-20 br2 mb2 bg-light-gray pa2"></textarea>
+    </div>
+</form>
+Num lines: {txtFriendlyNames.split('\n').length}
